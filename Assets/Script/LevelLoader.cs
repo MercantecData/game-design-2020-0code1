b186726bc11b6ed7cd19fc19eb8ef88166dbd9e0
An Tran Nguyen <anxx0041@elevcampus.dk>
2020-06-23 19:02:39 +0200

    intro scene

---
+++ Assets/Script/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+    // Update is called once per frame
+
+    public void LoadNextLevel()
+    {
+        //StartCoroutine((string)LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerable LoadLevel (int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
